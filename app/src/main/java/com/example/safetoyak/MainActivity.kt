be675eae5efa3097da51a08e47d9dd0a6f9e398f
David <pdstu25@gmail.com>
2022-04-25 16:07:33 -0500

    Added strings for warning title/message, Added warningMessage() function to start onCreate.

--- app/src/main/java/com/example/safetoyak/MainActivity.kt
+++ app/src/main/java/com/example/safetoyak/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.safetoyak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.safetoyak.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Set initial warning message
+        warningMessage()
+    }
+
+    //Set initial warning on startup
+    fun warningMessage() {
+        val warningBuilder = AlertDialog.Builder(binding.root.context)
+
+        warningBuilder.setTitle(R.string.warning)
+        warningBuilder.setMessage(R.string.main_warningMessage)
+        warningBuilder.setPositiveButton(android.R.string.ok, null)
+        warningBuilder.show()
     }
 }
